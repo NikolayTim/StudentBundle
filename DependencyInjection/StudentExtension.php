8d22b9c8f05381d72b8c2ceb9c48d0adfd2c7bd6
Nikolay <ntimakov@mail.ru>
2021-11-17 20:33:14 +0300

    change config

--- DependencyInjection/StudentExtension.php
+++ DependencyInjection/StudentExtension.php
@@ -12,6 +12,9 @@ class StudentExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('doctrine.yaml');
+        $loader->load('api_platform.yaml');
+        $loader->load('old_sound_rabbit_mq.yaml');
         $loader->load('services.yaml');
     }
 }
